be09d78ef8e2e43e8f1744a31a6450e5f488d349
NeuraXmy <neuraxmy@outlook.com>
2025-07-06 19:16:49 +0800

    修复查卡bfes

--- src/plugins/sekai/common.py
+++ src/plugins/sekai/common.py
@@ -89,14 +89,14 @@ CARD_RARE_NAMES = [
     ("rarity_birthday", "生日", "生日卡", "bd"),
 ]
 CARD_SUPPLIES_NAMES = [
-    ("all_limited", "限定", "限"),
-    ("not_limited", "非限", "非限定"),
-    ("term_limited", "期间限定", "期间"),
-    ("colorful_festival_limited", "fes", "fes限", "fes限定", "Fes", "Fes限定"),
-    ("bloom_festival_limited", "新fes", "新fes限", "新fes限定", "新Fes", "新Fes限定"
+    ("bloom_festival_limited", "新fes", "新fes限", "新fes限定", "新Fes", "新Fes限定",
      "bfes", "bfes限", "bfes限定"),
     ("unit_event_limited", "wl", "wl限", "wl限定", "worldlink", "worldlink限定", "WL"),
     ("collaboration_limited", "联动", "联动限定"),
+    ("colorful_festival_limited", "fes", "fes限", "fes限定", "Fes", "Fes限定"),
+    ("not_limited", "非限", "非限定"),
+    ("term_limited", "期间限定", "期间"),
+    ("all_limited", "限定", "限"),
 ]
 CARD_SUPPLIES_SHOW_NAMES = {
     "term_limited": "期间限定",
