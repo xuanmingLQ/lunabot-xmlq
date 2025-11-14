991742267fc273edcb15ee221199b08703473e64
NeuraXmy <neuraxmy@outlook.com>
2025-11-14 22:53:14 +0800

    修复无法响应回复消息和@消息的问题

--- src/plugins/utils/handler.py
+++ src/plugins/utils/handler.py
@@ -25,6 +25,9 @@ GROUP_MEMBER_NAME_CACHE_EXPIRE_SECONDS_CFG = global_config.item('group_member_na
 
 # 修改adapters.onebot.v11.bot中的函数，使得event处理消息的时候获取to_me时不会把at和reply去掉
 
+_at_me_msg_ids = set()
+_reply_me_msg_ids = set()
+
 async def _mp_check_reply(bot: "Bot", event: MessageEvent) -> None:
     """检查消息中存在的回复，去除并赋值 `event.reply`, `event.to_me`。
 
@@ -49,14 +52,16 @@ async def _mp_check_reply(bot: "Bot", event: MessageEvent) -> None:
         # ensure string comparation
         if str(event.reply.sender.user_id) == str(event.self_id):
             event.to_me = True
-        # del event.message[index]
+        del event.message[index]
+        _reply_me_msg_ids.add(event.message_id)
 
-        # if (
-        #     len(event.message) > index
-        #     and event.message[index].type == "at"
-        #     and event.message[index].data.get("qq") == str(event.reply.sender.user_id)
-        # ):
-        #     del event.message[index]
+        if (
+            len(event.message) > index
+            and event.message[index].type == "at"
+            and event.message[index].data.get("qq") == str(event.reply.sender.user_id)
+        ):
+            del event.message[index]
+            _at_me_msg_ids.add(event.message_id)
 
     # if len(event.message) > index and event.message[index].type == "text":
     #     event.message[index].data["text"] = event.message[index].data["text"].lstrip()
@@ -92,36 +97,38 @@ def _mp_check_at_me(bot: "Bot", event: MessageEvent) -> None:
         # check the first segment
         if _is_at_me_seg(event.message[0]):
             event.to_me = True
-            # event.message.pop(0)
-            # if event.message and event.message[0].type == "text":
-            #     event.message[0].data["text"] = event.message[0].data["text"].lstrip()
-            #     if not event.message[0].data["text"]:
-            #         del event.message[0]
-            #         pass
-            # if event.message and _is_at_me_seg(event.message[0]):
-            #     event.message.pop(0)
-            #     if event.message and event.message[0].type == "text":
-            #         event.message[0].data["text"] = (
-            #             event.message[0].data["text"].lstrip()
-            #         )
-            #         if not event.message[0].data["text"]:
-            #             del event.message[0]
+            event.message.pop(0)
+            _at_me_msg_ids.add(event.message_id)
+            if event.message and event.message[0].type == "text":
+                event.message[0].data["text"] = event.message[0].data["text"].lstrip()
+                if not event.message[0].data["text"]:
+                    del event.message[0]
+                    pass
+            if event.message and _is_at_me_seg(event.message[0]):
+                event.message.pop(0)
+                if event.message and event.message[0].type == "text":
+                    event.message[0].data["text"] = (
+                        event.message[0].data["text"].lstrip()
+                    )
+                    if not event.message[0].data["text"]:
+                        del event.message[0]
 
-        # if not event.to_me:
-        #     # check the last segment
-        #     i = -1
-        #     last_msg_seg = event.message[i]
-        #     if (
-        #         last_msg_seg.type == "text"
-        #         and not last_msg_seg.data["text"].strip()
-        #         and len(event.message) >= 2
-        #     ):
-        #         i -= 1
-        #         last_msg_seg = event.message[i]
+        if not event.to_me:
+            # check the last segment
+            i = -1
+            last_msg_seg = event.message[i]
+            if (
+                last_msg_seg.type == "text"
+                and not last_msg_seg.data["text"].strip()
+                and len(event.message) >= 2
+            ):
+                i -= 1
+                last_msg_seg = event.message[i]
 
-        #     if _is_at_me_seg(last_msg_seg):
-        #         event.to_me = True
-        #         del event.message[i:]
+            if _is_at_me_seg(last_msg_seg):
+                event.to_me = True
+                del event.message[i:]
+                _at_me_msg_ids.add(event.message_id)
 
         if not event.message:
             event.message.append(MessageSegment.text(""))
@@ -342,6 +349,11 @@ def process_msg_segs(msg: list[dict] | Message, event: MessageEvent | None = Non
     对消息段列表进行一些处理以保证各个bot后端的兼容性
     """
     ret = []
+    if event:
+        if event.message_id in _reply_me_msg_ids:
+            ret.append({ 'type': 'reply', 'data': { 'id': str(event.message_id) } })
+        if event.message_id in _at_me_msg_ids:
+            ret.append({ 'type': 'at', 'data': { 'qq': str(event.self_id) } })
     for seg in msg:
         stype = seg['type'] if isinstance(seg, dict) else str(seg.type)
         sdata = seg['data'] if isinstance(seg, dict) else dict(seg.data)
