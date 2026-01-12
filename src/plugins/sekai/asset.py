738a4ec2ce2a2da24e56c7e6a129eb9eaa7b98b1
NeuraXmy <neuraxmy@outlook.com>
2026-01-12 18:58:08 +0800

    优化master加载失败时的报错

--- src/plugins/sekai/asset.py
+++ src/plugins/sekai/asset.py
@@ -284,6 +284,9 @@ class MasterDataManager:
             source = await db_mgr.get_latest_source()
             if get_version_order(self.version.get(region, DEFAULT_VERSION)) < get_version_order(source.version):
                 await self._download_from_db(region, source)
+            # 检查是否存在，如果仍然不存在则报错
+            if self.data.get(region) is None:
+                raise Exception(f"获取 MasterData [{region}.{self.name}] 的数据失败")
 
     async def get_data(self, region: str):
         """
